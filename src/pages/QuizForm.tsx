f1984077810403ab4e4526ed645211cf3126c51b
Mostafa Aly <mostafa.aly25195@gmail.com>
2022-12-04 20:11:42 +0200

    make fields cotrolled + ability to add new question

--- src/pages/QuizForm.tsx
+++ src/pages/QuizForm.tsx
@@ -1,18 +1,82 @@
-import { FC } from "react";
+import { ChangeEvent, FC, useState } from "react";
+import { Quiz } from "src/interfaces/Quiz";
+import { Question } from "src/interfaces/Question";
 
 const QuizForm: FC = () => {
+  const [quiz, setQuiz] = useState<Quiz>({
+    id: Date.now(),
+    created: "",
+    description: "",
+    modified: "",
+    questions_answers: [],
+    score: 0,
+    title: "",
+    url: "",
+  });
+
+  const handleChangeQuiz = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ): void => {
+    const { name, value } = e.target;
+    setQuiz((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleAddQuestion = (): void => {
+    const question: Question = {
+      answer_id: null,
+      answers: [],
+      feedback_false: "",
+      feedback_true: "",
+      id: Date.now(),
+      text: "",
+    };
+    setQuiz((prevState) => ({
+      ...prevState,
+      questions_answers: [...prevState.questions_answers, question],
+    }));
+  };
+
   return (
     <div>
       <h1>Create quiz</h1>
       <form>
-        <input placeholder="Title" />
+        <input
+          placeholder="Title"
+          value={quiz.title}
+          onChange={handleChangeQuiz}
+          name="title"
+        />
         <br />
-        <textarea placeholder="Description" rows={7} />
+        <textarea
+          placeholder="Description"
+          rows={7}
+          value={quiz.description}
+          onChange={handleChangeQuiz}
+          name="description"
+        />
         <br />
-        <input type="number" placeholder="Score" />
+        <input
+          type="number"
+          placeholder="Score"
+          value={quiz.score}
+          onChange={handleChangeQuiz}
+          name="score"
+        />
         <br />
-        <input placeholder="Youtube video" type="url" />
+        <input
+          placeholder="Youtube video"
+          type="url"
+          value={quiz.url}
+          onChange={handleChangeQuiz}
+          name="url"
+        />
         <br />
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <h3>Questions</h3>
+          <button type="button" onClick={handleAddQuestion}>
+            Add
+          </button>
+        </div>
       </form>
     </div>
   );
