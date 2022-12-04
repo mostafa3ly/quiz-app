2659b593c769ba0adc1f9c7539de3bbd0ac99cf3
Mostafa Aly <mostafa.aly25195@gmail.com>
2022-12-04 20:00:14 +0200

    add basic info for quiz create

--- src/pages/QuizForm.tsx
+++ src/pages/QuizForm.tsx
@@ -1,7 +1,21 @@
 import { FC } from "react";
 
 const QuizForm: FC = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Create quiz</h1>
+      <form>
+        <input placeholder="Title" />
+        <br />
+        <textarea placeholder="Description" rows={7} />
+        <br />
+        <input type="number" placeholder="Score" />
+        <br />
+        <input placeholder="Youtube video" type="url" />
+        <br />
+      </form>
+    </div>
+  );
 };
 
 export default QuizForm;
